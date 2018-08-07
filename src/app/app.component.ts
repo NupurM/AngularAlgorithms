1b328fa97d753e8c9ec35a76f08786ca72c1968b
NM <NM>
2018-08-07 00:50:59 -0400

    Add HTML5 semantic tags to app.html and create a counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Algorithms';
+  description = 'Algorithems written in TypeScript and displayed using the Angular Framework';
 }
