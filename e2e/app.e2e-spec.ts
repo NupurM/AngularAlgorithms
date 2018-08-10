90c7bd7b47c0615c229f02bf5dc5375644d1f111
NM <NM>
2018-08-10 01:06:53 -0400

    Added End-2-End tests for Counter

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,46 @@ describe('angular-algorithms App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display page title', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getTitleText()).toEqual('Angular Algorithms');
+  });
+
+  it('should have a counter', () => {
+    page.navigateTo();
+    expect(page.getCounterValue()).toBeDefined();
+  });
+
+  it('should display 0 as counter', () => {
+    page.navigateTo();
+    expect(page.getCounterValue()).toBe('0');
+  });
+
+  it('should increment the counter by 2', () => {
+    page.navigateTo();
+
+    page.getCounterIncrementButton().click();
+    page.getCounterIncrementButton().click();
+
+    expect(page.getCounterValue()).toBe('2');
+  });
+
+  it('should increment the counter by 2, decrement by 3 and then reset to 0', () => {
+    page.navigateTo();
+
+    page.getCounterIncrementButton().click();
+    page.getCounterIncrementButton().click();
+
+    expect(page.getCounterValue()).toBe('2');
+
+    page.getCounterDecrementButton().click();
+    page.getCounterDecrementButton().click();
+    page.getCounterDecrementButton().click();
+
+    expect(page.getCounterValue()).toBe('-1');
+
+    page.getCounterResetButton().click();
+
+    expect(page.getCounterValue()).toBe('0');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,24 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  getTitleText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getCounterValue() {
+    return element(by.id('counterValue')).getText();
+  }
+
+  getCounterIncrementButton() {
+    return element(by.cssContainingText('button', 'Increment'));
+  }
+
+  getCounterDecrementButton() {
+    return element(by.cssContainingText('button', 'Decrement'));
+  }
+
+  getCounterResetButton() {
+    return element(by.cssContainingText('button', 'Reset'));
+  }
+
 }
