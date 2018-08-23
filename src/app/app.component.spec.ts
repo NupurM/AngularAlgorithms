6a71db52eaf2a565fe1dbbd9e4a7566347495e69
NM <NM>
2018-08-22 21:46:35 -0400

    Fix unit tests for App Component and Header Component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,46 +1,46 @@
-import { TestBed, async } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement, Component } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { BinarySearchComponent } from './binary-search/binary-search.component';
-import { CounterComponent } from './counter/counter.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { StringReversalComponent } from './string-reversal/string-reversal.component';
 
-import { ApiService } from './api.service';
+@Component({
+  selector: 'app-header',
+  template: ''
+})
+class HeaderStubComponent { }
+
+@Component({
+  selector: 'app-footer',
+  template: ''
+})
+class FooterStubComponent { }
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        BinarySearchComponent,
-        CounterComponent,
-        FooterComponent,
-        HeaderComponent,
-        HomeComponent,
-        StringReversalComponent,
-      ],
-      providers: [ApiService],
-      imports: [HttpClientTestingModule],
+        HeaderStubComponent,
+        FooterStubComponent],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  xit(`should have as title 'Angular Algorithms'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular Algorithms');
-  }));
-  xit('should render title in an h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+
+    component = fixture.componentInstance;
+
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Angular Algorithms');
+  });
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
   }));
 });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,25 +1,37 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { DebugElement } from '@angular/core';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have a title as 'Angular Algorithms'`, async(() => {
+    expect(component.title).toEqual('Angular Algorithms');
+  }));
+
+  it('should render title in an h1 tag', async(() => {
+    expect(de.nativeElement.querySelector('h1').textContent).toContain('Angular Algorithms');
+  }));
+
 });
